eddd389fb19cc24b0ae0d500ea33239d0f4aa5c9
Bombel <bombel@bombel.com>
2020-12-17 17:37:02 +0100

    Instal nodemon

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Go to nodejs.org and download nodejs
 Create new folder and accepted all steps
 
 ### Create index.js
+
+### Learn about GET, PUT, POST, DELETE
+GET - request asks for data from the web server
+POST - request sends data to the web server
+PUT - request updates an existing resource
+DELETE - request delets a resource from the web server
--- first_server/index.js
+++ first_server/index.js
@@ -4,9 +4,12 @@ const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((reg, res) => {
+    console.log(reg);
+
     res.end('Welcome to Node!');
 })
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at ${hostname}:${port}`)
+    // console.log(`Server running at ${hostname}:${port}`)
+    console.log("Server running at " + hostname + ":" + port)
 })
