ee29b226c13d4fd84458747709227d4cf8732f8f
Bombel <bombel@bombel.com>
2020-12-19 16:54:22 +0100

    Use function send and json to display url in browser

--- fortunes_api/index.js
+++ fortunes_api/index.js
@@ -1,14 +1,22 @@
 // express -> it is the function with an argument 'express' which we can use
 const express = require('express');
+const fortunes = require('./fortunes_api.json')
 
 const port = 3000;
 // app -> function with express application witch we can configuration
 const app = express();
 
+console.log('test nodemon')
+
 // req -> request, res -> response
 // get -> function witch check url with express function
 app.get('/fortunes', (req, res) => {
-    console.log('requesting fortunes');
+
+    // // send -> function response object
+    // res.send('requesting fortunes')
+
+    // json -> function response file
+    res.json(fortunes);
 })
 
 // listen -> function listening
