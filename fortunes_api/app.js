f3e06686a2eec8dc8fd00d9227659b898c11eaf3
Bombel <bombel@bombel.com>
2020-12-21 21:18:04 +0100

    Add post metode

--- fortunes_api/app.js
+++ fortunes_api/app.js
@@ -1,11 +1,13 @@
 // express -> it is the function with an argument 'express' which we can use
+const fs = require('fs');
 const express = require('express');
-const fortunes = require('./fortunes_api.json')
+const fortunes = require('./fortunes_api.json');
+const bodyParser = require('body-parser');
 
 // app -> function with express application witch we can configuration
 const app = express();
 
-console.log('test nodemon')
+app.use(bodyParser.json());
 
 // req -> request, res -> response
 // get -> function witch check url with express function
@@ -30,6 +32,24 @@ app.get('/fortunes/random', (req, res) => {
 app.get('/fortunes/random/:id', (req, res) => {
     // find -> function witch find specific element with individual argument
     res.json(fortunes.find(f => f.id == req.params.id));
+})
+
+app.post('/fortunes', (req, res) => {
+    console.log(req.body);
+
+    const { message, lucky_number, spirit_animal } = req.body;
+    const fortune_ids = fortunes.map(f => f.id)
+    const fortune = {
+        id: (fortune_ids > 0 ? Math.max(...fortune_ids) : 0) + 1,
+        message,
+        lucky_number,
+        spirit_animal
+    };
+    const new_fortunes = fortunes.concat(fortune);
+
+    fs.writeFile('data/fortunes.json', JSON.stringify(new_fortunes), err => console.log(err ));
+
+    res.json(new_fortunes);
 })
 
 module.exports = app;
