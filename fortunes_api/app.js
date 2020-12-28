c5b546b4bc0419d478b98780ebd4f5d69a50251b
Bombel <bombel@bombel.com>
2020-12-28 10:16:49 +0100

    Learn put and delete method

--- fortunes_api/app.js
+++ fortunes_api/app.js
@@ -1,7 +1,8 @@
 // express -> it is the function with an argument 'express' which we can use
 const fs = require('fs');
 const express = require('express');
-const fortunes = require('./fortunes_api.json');
+let fortunes = require('./fortunes_api.json');
+console.log('requiring fortunes data')
 const bodyParser = require('body-parser');
 
 // app -> function with express application witch we can configuration
@@ -34,21 +35,65 @@ app.get('/fortunes/random/:id', (req, res) => {
     res.json(fortunes.find(f => f.id == req.params.id));
 })
 
+const writeFortunes = json => {
+    fs.writeFile('./fortunes_api.json', JSON.stringify(json), err => console.log(err ));
+}
+
+const readFortunes = () => {
+    return fs.readFileSync('./fortunes_api.json', 'utf8')
+}
+
 app.post('/fortunes', (req, res) => {
-    console.log(req.body);
+    // let fortunesFromFile = [];
+    // for (let i = 0; i < 100; i++) {
+    //     fortunesFromFile.push({ id: i, message: i, luckyNumber: i, spiritAnimal: i })
+    // }
+    const fortunesFromFile = JSON.parse(readFortunes())
 
-    const { message, lucky_number, spirit_animal } = req.body;
-    const fortune_ids = fortunes.map(f => f.id)
+    const { message, luckyNumber, spiritAnimal } = req.body
+
+    const fortuneIds = fortunesFromFile.map(f => f.id)
+    //...[1, 2, 3] => 1, 2, 3
+    const maxId = Math.max(...fortuneIds)
+    const nextId = fortuneIds.length === 0 ? 1 : maxId + 1
+
     const fortune = {
-        id: (fortune_ids > 0 ? Math.max(...fortune_ids) : 0) + 1,
+        id: nextId,
         message,
-        lucky_number,
-        spirit_animal
-    };
-    const new_fortunes = fortunes.concat(fortune);
+        luckyNumber,
+        spiritAnimal
+    }
+    const updatedFortunes = fortunesFromFile.concat(fortune)
 
-    fs.writeFile('data/fortunes.json', JSON.stringify(new_fortunes), err => console.log(err ));
+    writeFortunes(updatedFortunes)
+    res.json(fortune)
+})
 
+app.put('/fortunes/:id', (req, res) => {
+    const fortunesFromFile = JSON.parse(readFortunes())
+    const { id } = req.params;
+
+    const oldFortune = fortunesFromFile.find(f => f.id == id);
+
+    const { message, luckyNumber, spiritAnimal } = req.body
+
+    if (oldFortune) {
+        if (message) oldFortune.message = message;
+        if (luckyNumber) oldFortune.luckyNumber = luckyNumber;
+        if (spiritAnimal) oldFortune.spiritAnimal = spiritAnimal;
+    }
+
+    writeFortunes(fortunesFromFile);
+    res.json(fortunesFromFile);
+})
+
+app.delete('/fortunes/:id', (req, res) => {
+    const fortunesFromFile = JSON.parse(readFortunes())
+    const { id } = req.params;
+
+    const new_fortunes = fortunesFromFile.filter(f => f.id != id);
+
+    writeFortunes(new_fortunes);
     res.json(new_fortunes);
 })
 
