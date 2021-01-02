5c512be61b4368d63ccfeb6e4dccbfe1d1137bb1
Bombel <bombel@bombel.com>
2021-01-02 23:51:03 +0100

    Make a id

--- monsters_api/api/app.js
+++ monsters_api/api/app.js
@@ -4,8 +4,17 @@ const pool = require('./pool');
 const app = express();
 
 app.get('/monsters', (request, response, next) => {
-    console.log('GET /monsters');
-    pool.query('SELECT * FROM monsters WHERE id = 1', (err, res) => {
+    pool.query('SELECT * FROM monsters ORDER BY id ASC', (err, res) => {
+        if (err) return  next(err);
+
+        response.json(res.rows);
+    });
+});
+
+app.get('/monsters/:id', (request, response, next) => {
+    console.log('/monsters:id');
+    const { id } = request.params;
+    pool.query('SELECT * FROM monsters WHERE id = $1', [id], (err, res) => {
         if (err) return  next(err);
 
         response.json(res.rows);
